50d8448097f02fc2b6e0b2e8f443deaaad11f31a
Eric Bailey <eric@ericb.me>
2015-06-01 00:09:51 -0500

    get "/hi"

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
+import Data.Maybe (fromMaybe)
+import Data.Monoid (mconcat)
 import Web.Scotty
 
 main :: IO ()
-main = scotty 3000 $
+main = scotty 3000 $ do
   get "/" $ text "Hello, world!"
+  get "/hi" $ do
+    agent <- header "User-Agent"
+    text $ mconcat ["Hello, ", fromMaybe "world" agent, "!"]
